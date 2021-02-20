4b053a8f538b13a1e03d673993fcd13e97454401
Karthik-ctrl-A <saikarthik4me@gmail.com>
2021-02-20 22:29:17 +0530

    initial commit - with jumbotron

---
+++ src/global-styles.js
@@ -0,0 +1,15 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+    html,body{
+        font-family: 'Poppins', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: black;
+        scroll-behavior: smooth;
+        color: #333333;
+        font-size: 16px;
+    }
+
+`;
